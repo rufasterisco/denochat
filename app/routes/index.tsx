7e5538b3b16f0229c4df671779070550392e0a7e
Francesco <francesco.ruffato@nexyo.io>
2023-09-04 20:05:22 +0200

    initial cleanup of example files

---
+++ app/routes/index.tsx
@@ -0,0 +1,29 @@
+import { useSignal } from "@preact/signals";
+// import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  // const count = useSignal(3);
+
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <h1 class="text-4xl font-bold">We can finally chat</h1>
+      <input type="text"></input>
+      {
+        /* <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+        class="my-6"
+        src="/logo.svg"
+        width="128"
+        height="128"
+        alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
+        <Counter count={count} />
+      </div> */
+      }
+    </div>
+  );
+}
