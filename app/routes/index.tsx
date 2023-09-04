a481d39e7f780fdfe5227ed3b7b6bdd7411f8761
Francesco <francesco.ruffato@nexyo.io>
2023-09-05 00:14:20 +0200

    css improvements

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,16 +13,33 @@ export const handler: Handlers = {
 export default async function Page() {
   const messages = await database.findAllMessages();
   return (
-    <div>
-      <form method="POST">
-        <input type="text" name="message"></input>
-        <button type="submit">insert</button>
-      </form>
-      {
-        <ul>
-          {messages.map((item) => <li key={item._id}>{item.message}</li>)}
+    <div className="flex flex-col h-screen bg-gray-100">
+      <div className="flex-1 overflow-y-auto p-4">
+        <ul className="space-y-4">
+          {messages.map((item) => (
+            <li key={item._id} className="p-4 rounded bg-white shadow">
+              {item.message}
+            </li>
+          ))}
         </ul>
-      }
+      </div>
+      <form
+        method="POST"
+        className="flex items-center p-4 bg-white border-t border-gray-200"
+      >
+        <input
+          type="text"
+          name="message"
+          className="flex-1 p-2 rounded border shadow-inner"
+          placeholder="Type your message"
+        />
+        <button
+          type="submit"
+          className="ml-4 px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600"
+        >
+          Insert
+        </button>
+      </form>
     </div>
   );
 }
