9fc280b1bda04cc6c79964e5f9647f71cdbf7f76
Francesco <francesco.ruffato@nexyo.io>
2023-09-05 02:36:04 +0200

    inline css -> tailwind css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { database, MessageSchema } from "../db/mongo.ts";
+import NameGenerator from "../islands/NameGenerator.tsx";
 import ScrollIntoViewComponent from "../islands/ScrollIntoViewComponent.tsx";
 import { formatTimestampToRelativeTime } from "../utils/time.ts";
 
@@ -20,24 +21,34 @@ export const handler: Handlers = {
 export default async function Page() {
   const messages: MessageSchema[] = await database.findAllMessages();
   return (
-    <div className="flex flex-col h-screen bg-gray-100">
-      <div className="flex-1 overflow-y-auto p-4">
+    <div className="flex flex-col h-screen bg-[#8ecae6]">
+      <div className="fixed top-0 left-0 right-0 flex items-center justify-between z-50 text-xl font-bold p-4 text-[#023047] bg-[#219ebc]">
+        <NameGenerator></NameGenerator>
+        {/* Welcome Here */}
+        <form method="POST" action="/delete-chat">
+          <button
+            className="p-2 rounded-full focus:outline-none focus:ring focus:ring-[#fb8500]"
+            style={{ backgroundColor: "#ffb703", color: "#023047" }}
+          >
+            Delete Chat
+          </button>
+        </form>
+      </div>{" "}
+      <div className="flex-1 overflow-y-auto p-4 mt-16 bg-opacity-50 bg-[#8ecae6]">
         <ul className="space-y-4">
           {messages.map((item) => (
             <li
               key={item._id}
-              className="block px-2.5 py-4"
+              className="block px-2.5 py-4 text-[#023047]"
               style={{ width: "max-content" }}
             >
-              <div className="text-sm text-gray-600 mb-1">
-                {/* Placeholder for sender's name */}
-                {"Sender Name"}
+              <div className="text-sm mb-1 text-[#023047] font-bold">
+                Sender Name
               </div>
-              <div className="rounded bg-white shadow text-base px-4 py-2">
+              <div className="inline-block text-base px-4 py-2 rounded-[16px] bg-white shadow text-[#023047]">
                 {item.message}
               </div>
-              <div className="text-xs text-gray-500 mt-1">
-                {/* Placeholder for timestamp */}
+              <div className="text-xs mt-1 pt-2 text-[#023047]">
                 {formatTimestampToRelativeTime(item._id.getTimestamp()) ||
                   "Timestamp"}
               </div>
@@ -46,28 +57,32 @@ export default async function Page() {
         </ul>
         <ScrollIntoViewComponent />
       </div>
-      <form
-        method="POST"
-        action="/delete-chat"
-      >
-        <button className="fixed top-4 right-4 z-50 p-2 rounded-full bg-red-600 text-white hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-200">
+      {
+        /* <form method="POST" action="/delete-chat">
+        <button
+          className="fixed top-4 right-4 z-50 p-2 rounded-full focus:outline-none focus:ring focus:ring-[#fb8500]"
+          style={{ backgroundColor: "#ffb703", color: "#023047" }}
+        >
           Delete Chat
         </button>
-      </form>
+      </form> */
+      }
       <form
         method="POST"
-        className="flex items-center p-4 bg-white border-t border-gray-200"
+        className="flex items-center p-4 bg-[#8ecae6] border-t border-[#023047]"
       >
         <input
           type="text"
           name="message"
-          className="flex-1 p-2 rounded border shadow-inner"
+          className="flex-1 p-2 rounded border shadow-inner text-[#023047]"
+          style={{ borderColor: "#023047" }}
           placeholder="Type your message"
           autoFocus
         />
         <button
           type="submit"
-          className="ml-4 px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600"
+          className="ml-4 px-4 py-2 rounded focus:outline-none focus:ring focus:ring-[#fb8500]"
+          style={{ backgroundColor: "#90e0ef", color: "#023047" }}
         >
           Send
         </button>
